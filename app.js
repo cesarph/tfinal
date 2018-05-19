316becce057401c96ec91cf972671b925696eb16
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-19 18:45:31 -0500

    Se añade el archivo .env y el sdk de watson

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require('./routes'));
+app.use('/', require('./routes'));
 
 const server = app.listen( process.env.PORT || 3000, () => 
     console.log('Listening on port ' + server.address().port)
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/chatbot', require('./chatbot'));
+
 router.use('/personality', require('./personality'));
 
 module.exports = router;    
