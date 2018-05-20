f1a98e131e53c82f5c0f2444aec3eac6998d57df
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-19 23:27:37 -0500

    Se añade la ruta de text-to-speech

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 
 router.use('/personality', require('./personality'));
+router.use('/text-to-speech', require('./text-to-speech'));
 
 module.exports = router;    
