60bb65b2e800a5a4d322f22b436f4b18c57ada5d
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-22 14:51:41 -0500

    Se añade headers para aceptar peticiones externas

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+router.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 router.use('/api', require('./api'));
 
 module.exports = router;
