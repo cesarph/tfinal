12c09922d01b151ab46bddd63862db48e7a5404f
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-22 21:27:21 -0500

    Cambia vr-poster a visual-recognition y se agrega nueva ruta
    Se cambia el nombre del archivo vr-poster debido a que se iba a ocupar
    el mismo servicio para otra ruta. También se cambiaron las rutas para
    que sean más descriptivas

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 router.use('/personality', require('./personality'));
 router.use('/text-to-speech', require('./text-to-speech'));
-router.use('/vr-poster', require('./vr-poster'));
+router.use('/visual-recognition', require('./visual-recognition'));
 
 module.exports = router;    
--- routes/api/visual-recognition.js
+++ routes/api/visual-recognition.js
@@ -8,11 +8,8 @@ const visualRecognition = new VisualRecognitionV3({
   version: '2018-03-19'
 });
 
-
-
-
-router.post('/:id', async (req, res) => {
-    const result = {};
+router.post('/poster/:id', async (req, res) => {
+    const result = { id: req.params.id };
     const params = {
         images_file: fs.createReadStream(`./client/src/assets/postersypendones/${req.params.id}.jpg`),
         accept_language: 'es'
@@ -28,7 +25,7 @@ router.post('/:id', async (req, res) => {
                         min: age.min, 
                         max: age.max 
                     },
-                    gender: gender.gender 
+                    gender: (gender.gender == 'MALE')? "Hombre" : "Mujer"
                 }
             ));
 
@@ -50,6 +47,23 @@ router.post('/:id', async (req, res) => {
     result.objects = await classify;
 
     res.json(result);
+});
+
+router.post('/food', (req, res) => {
+    const params = {
+        images_file: fs.createReadStream('prueba.jpg'),
+        accept_language: 'es',
+        classifier_ids: ["Areasdecomida_768439822"]
+      };
+
+
+    visualRecognition.classify(params, (err, watsonResp) => {
+        if (err) return console.log(err);
+        
+        res.json(watsonResp);
+    });
+
+    
 });
 
 module.exports = router;
