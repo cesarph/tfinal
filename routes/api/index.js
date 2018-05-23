8c42545aafa4a5ea85a8e3a748a75fc4aa0db8f0
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-23 14:19:03 -0500

    se borra la ruta de text-to-speech

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 router.use('/personality', require('./personality'));
-router.use('/text-to-speech', require('./text-to-speech'));
 router.use('/visual-recognition', require('./visual-recognition'));
 router.use('/chatbot', require('./chatbot'));
 router.use('/translator', require('./translator'));
