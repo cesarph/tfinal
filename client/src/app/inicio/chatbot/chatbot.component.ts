7a3033f4d821bd89082a47a6bbd130504dcfd04f
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-23 08:51:35 -0500

    Se añade el componente chatbot con su funcionalidad

---
+++ client/src/app/inicio/chatbot/chatbot.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppobservableService } from '../../services/appobservable.service';
+
+@Component({
+  selector: 'app-chatbot',
+  templateUrl: './chatbot.component.html',
+  styleUrls: ['./chatbot.component.scss']
+})
+
+export class ChatbotComponent implements OnInit {
+
+  public form_success = false;
+  public initialMessage = "";
+  public messages = [];
+
+  public question = 
+  {
+      "message": null,  
+  }
+
+  constructor(private observableService: AppobservableService) { }
+
+  ngOnInit() {
+    const url = '/api/chatbot';
+    this.observableService.createService(url, {"message": ""})
+				.subscribe(result => {
+          this.initialMessage = result.response
+				},
+					error => { }
+				);
+  }
+
+  submitMessage(form: any) {
+		if (form.valid) {
+			console.log('valid');
+			const url = '/api/chatbot';
+			this.observableService.createService(url, this.question)
+				.subscribe(result => {
+          this.form_success = result;
+          this.messages = [...this.messages, result];
+          console.log(this.messages);
+				},
+					error => { }
+				);
+		} else {
+			console.log('invalid');
+		}
+	}
+}
