179fc42bfe2a03b58d9fc8bf9993aa073410296e
César Palazuelos <cesar_96_ph@hotmail.com>
2018-05-23 08:52:55 -0500

    Se crea el servicio Watson Assistan y se configura

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/personality', require('./personality'));
 router.use('/text-to-speech', require('./text-to-speech'));
 router.use('/visual-recognition', require('./visual-recognition'));
+router.use('/chatbot', require('./chatbot'));
 
 module.exports = router;    
